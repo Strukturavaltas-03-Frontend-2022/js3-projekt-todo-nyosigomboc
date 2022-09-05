0302d47402c188a1b94f5615eab017dff318c774
Gyozo Miklos <nyosdroid@gmail.com>
2022-09-06 00:01:18 +0200

    eslint, gitignore added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-import {addTodo, getTodosIfCheckedIs, getNumberIfCheckedIs, toggle, remove, clearPending, save, tryToReload} from './todo_db.js';
+import {
+  addTodo, getTodosIfCheckedIs, getNumberIfCheckedIs, toggle,
+  remove, clearPending, save, tryToReload,
+// eslint-disable-next-line import/extensions
+} from './todo_db.js';
 
 const dateDiv = document.querySelector('.date');
 const pendingNumber = document.querySelector('#pending_number');
@@ -7,13 +11,15 @@ const pendingList = document.querySelector('.pending_list');
 const completedList = document.querySelector('.completed_list');
 const buttHideComplete = document.querySelector('#butt_hide_complete');
 const newText = document.querySelector('input[name="new_todo"]');
-const emptyDiv = document.querySelector('#empty')
-const hasTodoDiv = document.querySelector('#has_todo')
+const emptyDiv = document.querySelector('#empty');
+const hasTodoDiv = document.querySelector('#has_todo');
 
 const showDate = () => {
-  const usDate = new Intl.DateTimeFormat('en-US', { weekday: 'long', year: 'numeric', month: '2-digit', day: '2-digit', });
+  const usDate = new Intl.DateTimeFormat('en-US', {
+    weekday: 'long', year: 'numeric', month: '2-digit', day: '2-digit',
+  });
   const date = usDate.format(new Date());
-  dateDiv.innerHTML = date.replaceAll('/','-').replace(',','<br />');
+  dateDiv.innerHTML = date.replaceAll('/', '-').replace(',', '<br />');
   // sample wants US format, but with wrong separators and an unnecessary line break
 };
 
@@ -21,54 +27,55 @@ const getNumbers = () => {
   const pending = getNumberIfCheckedIs(false);
   const completed = getNumberIfCheckedIs(true);
   const total = pending + completed;
-  return [pending, completed, total]
-}
+  return [pending, completed, total];
+};
 
 const updateNumbers = () => {
-  const [pending, completed, total] = getNumbers()
+  const [pending, completed, total] = getNumbers();
   const percentage = (total === 0) ? 0 : Math.round(completed * 100 / total);
   pendingNumber.innerHTML = pending;
   donePercentage.innerHTML = percentage;
-}
+};
 
 const getliHTML = (id, text, checked = false) => `
   <li>
     <input type="checkbox" id="cb_${id}" name="cb_${id}" ><label for="cb_${id}" ${checked ? '"checked"' : ''}>${text}</label>
     <div class="del" ><img src="img/delete.png" alt="delete" class="delete" ><div>
-  </li>`
-  // from here: https://www.flaticon.com/free-icon/delete_3395538
-  // FA is a lot bigger, no need to include that for one icon
-  
+  </li>`;
+// from here: https://www.flaticon.com/free-icon/delete_3395538
+// FA is a lot bigger, no need to include that for one icon
+
 const updatePending = () => {
   const html = getTodosIfCheckedIs(false)
     .map((todo) => getliHTML(todo.id, todo.text, false))
     .join('');
   pendingList.innerHTML = html;
-}
+};
 
 const updateDone = () => {
   const html = getTodosIfCheckedIs(true)
     .map((todo) => getliHTML(todo.id, todo.text, false))
     .join('\n');
   completedList.innerHTML = html;
-}
+};
 
 const updateDivVisibility = () => {
-  const [,,total] = getNumbers();
-  if(total===0) {
+  const [,, total] = getNumbers();
+  if (total === 0) {
     emptyDiv.style.display = 'block';
     hasTodoDiv.style.display = 'none';
   } else {
     emptyDiv.style.display = 'none';
     hasTodoDiv.style.display = 'block';
   }
-}
+};
 
 const update = () => {
   showDate();
   updateNumbers();
   updatePending();
   updateDone();
+  // eslint-disable-next-line no-use-before-define
   addListeners();
   updateDivVisibility();
   save();
@@ -84,7 +91,7 @@ function addListeners() {
     element.parentNode.children[element.parentNode.childElementCount - 1].addEventListener('click', () => {
       remove(myId);
       update();
-    })
+    });
   });
 }
 
@@ -96,14 +103,14 @@ document.querySelector('#butt_clear').addEventListener('click', () => {
 let completedVisible = true;
 const toggleComplete = () => {
   completedVisible = !completedVisible;
-  if(completedVisible) {
+  if (completedVisible) {
     completedList.style.display = 'block';
     buttHideComplete.innerHTML = 'Hide complete';
   } else {
     completedList.style.display = 'none';
     buttHideComplete.innerHTML = 'Show complete';
   }
-}
+};
 
 buttHideComplete.addEventListener('click', toggleComplete);
 
@@ -111,9 +118,9 @@ document.querySelector('.add_new_todo').addEventListener('click', () => {
   const newId = addTodo(newText.value);
   newText.value = '';
   update();
-  const newLi = document.getElementById(`cb_${newId}`).parentNode
+  const newLi = document.getElementById(`cb_${newId}`).parentNode;
   newLi.style.opacity = 0;
-  setTimeout(()=>{
+  setTimeout(() => {
     newLi.style.opacity = 1;
   }, 200);
 });
--- js/todo_db.js
+++ js/todo_db.js
@@ -3,60 +3,65 @@ let state = {
   todos: {
     1: {
       id: 1,
-      text: "Delete the default todos :)",
+      text: 'Delete the default todos :)',
       checked: false,
     },
     2: {
       id: 2,
-      text: "At least this has already been completed",
+      text: 'At least this has already been completed',
       checked: true,
     },
   },
 };
 
-const getNextId = () => state.sequence++;
+const getNextId = () => {
+  state.sequence += 1;
+  return state.sequence;
+};
 
 export const addTodo = (text) => {
   const newId = getNextId();
-  state.todos[newId]=({id: newId, text, checked: false, });
+  state.todos[newId] = ({ id: newId, text, checked: false });
   return newId;
-}
+};
 
-export const getTodosIfCheckedIs = (checked = false) => Object.values(state.todos).filter((todo) => todo.checked === checked);
+export const getTodosIfCheckedIs = (checked = false) => Object.values(state.todos)
+  .filter((todo) => todo.checked === checked);
 
-export const getNumberIfCheckedIs = (checked = false) => Object.values(state.todos).reduce((prev, todo) => todo.checked === checked ? prev + 1 : prev, 0);
+export const getNumberIfCheckedIs = (checked = false) => Object.values(state.todos)
+  .reduce((prev, todo) => (todo.checked === checked ? prev + 1 : prev), 0);
 
 export const toggle = (id) => {
   state.todos[id].checked = !state.todos[id].checked; // ^1
-}
+};
 
 export const clear = () => {
-  state = {sequence: 1, todos: {}};
+  state = { sequence: 1, todos: {} };
+};
+
+export const remove = (id) => {
+  if (state.todos.hasOwnProperty(id)) {
+    delete state.todos[id];
+  }
 };
 
 export const clearPending = () => {
   Object.entries(state.todos).forEach(([key, todo]) => {
-    if(!todo.checked) {
+    if (!todo.checked) {
       remove(key);
     }
   });
 };
 
-export const remove = (id) => {
-  if(state.todos.hasOwnProperty(id)){
-    delete state.todos[id];
-  }
-}
-
 const localStorageName = 'todoState';
 
 export const tryToReload = () => {
   const result = localStorage.getItem(localStorageName);
-  if(result !== null) {
+  if (result !== null) {
     state = JSON.parse(result);
   }
-}
+};
 
 export const save = () => {
   localStorage.setItem(localStorageName, JSON.stringify(state));
-}
+};
