6adedb2d54cc5d91b2ef127b58ad19a5d679ea2c
Gyozo Miklos <nyosdroid@gmail.com>
2022-09-05 07:44:58 +0200

    design could be improved, but it works

---
+++ js/main.js
@@ -0,0 +1,120 @@
+import {addTodo, getTodosIfCheckedIs, getNumberIfCheckedIs, toggle, remove, clearPending, save, tryToReload} from './todo_db.js';
+
+const dateDiv = document.querySelector('.date');
+const pendingNumber = document.querySelector('#pending_number');
+const donePercentage = document.querySelector('#done_percentage');
+const pendingList = document.querySelector('.pending_list');
+const completedList = document.querySelector('.completed_list');
+const buttHideComplete = document.querySelector('#butt_hide_complete');
+const newText = document.querySelector('input[name="new_todo"]');
+const emptyDiv = document.querySelector('#empty')
+const hasTodoDiv = document.querySelector('#has_todo')
+
+const showDate = () => {
+  const usDate = new Intl.DateTimeFormat('en-US', { weekday: 'long', year: 'numeric', month: '2-digit', day: '2-digit', });
+  const date = usDate.format(new Date());
+  dateDiv.innerHTML = date.replaceAll('/','-').replace(',','<br />');
+  // sample wants US format, but with wrong separators and an unnecessary line break
+};
+
+const getNumbers = () => {
+  const pending = getNumberIfCheckedIs(false);
+  const completed = getNumberIfCheckedIs(true);
+  const total = pending + completed;
+  return [pending, completed, total]
+}
+
+const updateNumbers = () => {
+  const [pending, completed, total] = getNumbers()
+  const percentage = (total === 0) ? 0 : Math.round(completed * 100 / total);
+  pendingNumber.innerHTML = pending;
+  donePercentage.innerHTML = percentage;
+}
+
+const getliHTML = (id, text, checked = false) => `
+  <li>
+    <input type="checkbox" id="cb_${id}" name="cb_${id}" ><label for="cb_${id}" ${checked ? '"checked"' : ''}>${text}</label>
+    <div class="del" ><img src="img/delete.png" alt="delete" class="delete" ><div>
+  </li>`
+
+const updatePending = () => {
+  const html = getTodosIfCheckedIs(false)
+    .map((todo) => getliHTML(todo.id, todo.text, false))
+    .join('');
+  pendingList.innerHTML = html;
+}
+
+const updateDone = () => {
+  const html = getTodosIfCheckedIs(true)
+    .map((todo) => getliHTML(todo.id, todo.text, false))
+    .join('\n');
+  completedList.innerHTML = html;
+}
+
+const updateDivVisibility = () => {
+  const [,,total] = getNumbers();
+  if(total===0) {
+    emptyDiv.style.display = 'block';
+    hasTodoDiv.style.display = 'none';
+  } else {
+    emptyDiv.style.display = 'none';
+    hasTodoDiv.style.display = 'block';
+  }
+}
+
+const update = () => {
+  showDate();
+  updateNumbers();
+  updatePending();
+  updateDone();
+  addListeners();
+  updateDivVisibility();
+  save();
+};
+
+function addListeners() {
+  document.querySelectorAll('input[type="checkbox"]').forEach((element) => {
+    const myId = element.id.slice('cb_'.length);
+    element.addEventListener('click', () => {
+      toggle(myId);
+      update();
+    });
+    element.parentNode.children[element.parentNode.childElementCount - 1].addEventListener('click', () => {
+      remove(myId);
+      update();
+    })
+  });
+}
+
+document.querySelector('#butt_clear').addEventListener('click', () => {
+  clearPending();
+  update();
+});
+
+let completedVisible = true;
+const toggleComplete = () => {
+  completedVisible = !completedVisible;
+  if(completedVisible) {
+    completedList.style.display = 'block';
+    buttHideComplete.innerHTML = 'Hide complete';
+  } else {
+    completedList.style.display = 'none';
+    buttHideComplete.innerHTML = 'Show complete';
+  }
+}
+
+buttHideComplete.addEventListener('click', toggleComplete);
+
+document.querySelector('.add_new_todo').addEventListener('click', () => {
+  const newId = addTodo(newText.value);
+  newText.value = '';
+  update();
+  const newLi = document.getElementById(`cb_${newId}`).parentNode
+  newLi.style.opacity = 0;
+  setTimeout(()=>{
+    newLi.style.opacity = 1;
+  }, 200);
+});
+
+tryToReload();
+update();
---
+++ js/todo_db.js
@@ -0,0 +1,62 @@
+let state = {
+  sequence: 3,
+  todos: {
+    1: {
+      id: 1,
+      text: "Pick",
+      checked: false,
+    },
+    2: {
+      id: 2,
+      text: "Done",
+      checked: true,
+    },
+  },
+};
+
+const getNextId = () => state.sequence++;
+
+export const addTodo = (text) => {
+  const newId = getNextId();
+  state.todos[newId]=({id: newId, text, checked: false, });
+  return newId;
+}
+
+export const getTodosIfCheckedIs = (checked = false) => Object.values(state.todos).filter((todo) => todo.checked === checked);
+
+export const getNumberIfCheckedIs = (checked = false) => Object.values(state.todos).reduce((prev, todo) => todo.checked === checked ? prev + 1 : prev, 0);
+
+export const toggle = (id) => {
+  state.todos[id].checked = !state.todos[id].checked; // ^1
+}
+
+export const clear = () => {
+  state = {sequence: 1, todos: {}};
+};
+
+export const clearPending = () => {
+  Object.entries(state.todos).forEach(([key, todo]) => {
+    if(!todo.checked) {
+      remove(key);
+    }
+  });
+};
+
+export const remove = (id) => {
+  if(state.todos.hasOwnProperty(id)){
+    delete state.todos[id];
+  }
+}
+
+const localStorageName = 'todoState';
+
+export const tryToReload = () => {
+  const result = localStorage.getItem(localStorageName);
+  if(result !== null) {
+    state = JSON.parse(result);
+  }
+}
+
+export const save = () => {
+  localStorage.setItem(localStorageName, JSON.stringify(state));
+}
